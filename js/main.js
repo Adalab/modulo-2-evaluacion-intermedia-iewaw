24ab1ae46b87cdb3a2ecda024a0f5aeefe69fd0d
iewaw <izabelawawrzyniak90@gmail.com>
2023-12-09 09:20:27 +0100

    adding js classes and getRandomNumber function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log('Hello, world!');
+const form = document.querySelector('.js__form');
+const input = document.querySelector('.js__input');
+const button = document.querySelector('.js__button');
+const hint = document.querySelector('.js__hint');
+const counter = document.querySelector('.js__counter');
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+const randomNumber = getRandomNumber(100);
+console.log(randomNumber);
