576d1a85407ba4068013e6cea3d74073a2d30cb5
iewaw <izabelawawrzyniak90@gmail.com>
2023-12-09 09:42:30 +0100

    obtaining number input

--- js/main.js
+++ js/main.js
@@ -6,9 +6,23 @@ const button = document.querySelector('.js__button');
 const hint = document.querySelector('.js__hint');
 const counter = document.querySelector('.js__counter');
 
+window.addEventListener('load', (event) => {
+    const randomNumber = getRandomNumber(100);
+    console.log(randomNumber);
+});
+
+input.addEventListener('input', getNumber);
+button.addEventListener('click', handleInput);
+
+function getNumber() {
+    console.log(parseInt(input.value));
+    return parseInt(input.value);
+}
+
+function handleInput() {
+    event.preventDefault();
+}
+
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
-
-const randomNumber = getRandomNumber(100);
-console.log(randomNumber);
