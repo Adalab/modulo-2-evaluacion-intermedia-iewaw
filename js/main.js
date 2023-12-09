4ccb38880e1e58f674c688e1fd93c17cfcc999dd
iewaw <izabelawawrzyniak90@gmail.com>
2023-12-09 10:16:37 +0100

    adding conditionals and finishing game logic

--- js/main.js
+++ js/main.js
@@ -1,22 +1,31 @@
 'use strict';
 
+//QUERY SELECTORS
 const form = document.querySelector('.js__form');
 const input = document.querySelector('.js__input');
 const button = document.querySelector('.js__button');
 const hint = document.querySelector('.js__hint');
 const counter = document.querySelector('.js__counter');
 
+//GLOBAL VARIABLES
 let userNumber = 0;
 let attempts = 0;
-let hintContent = '';
+let randomNumber = 0;
 
+//EVENT LISTENERS
 window.addEventListener('load', (event) => {
-    const randomNumber = getRandomNumber(100);
+    randomNumber = getRandomNumber(100);
     console.log(randomNumber);
 });
 
 input.addEventListener('input', getNumber);
+
 button.addEventListener('click', handleInput);
+
+//FUNCTIONS
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
 
 function getNumber() {
     userNumber = parseInt(input.value);
@@ -24,9 +33,17 @@ function getNumber() {
 
 function handleInput() {
     event.preventDefault();
-    console.log('Entered number: ' + userNumber)
-}
+    attempts++;
 
-function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
+    counter.innerHTML = 'Número de intentos: ' + attempts;
+
+    if (userNumber < 1 || userNumber > 100) {
+        hint.innerHTML = 'El número debe estar entre 1 y 100.';
+    } else if (userNumber < randomNumber) {
+        hint.innerHTML = 'Demasiado bajo.';
+    } else if (userNumber > randomNumber) {
+        hint.innerHTML = 'Demasiado alto.'
+    } else {
+        hint.innerHTML = 'Has ganado, campeona!!!'
+    }
 }
